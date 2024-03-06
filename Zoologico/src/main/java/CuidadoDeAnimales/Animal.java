f95e86e6875a0b14adc13768fdb3dcdd6ae8aa37
JorgeRomojaro11 <jromodel@myuax.com>
2024-03-06 11:41:24 +0100

    Creación Cuidado de animales y clase Animal

---
+++ Zoologico/src/main/java/CuidadoDeAnimales/Animal.java
@@ -0,0 +1,37 @@
+package CuidadoDeAnimales;
+
+public class Animal {
+    protected String nombre;
+    protected int salud;
+    protected String alimentacion;
+
+    public Animal(String nombre, int salud, String alimentacion) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.alimentacion = alimentacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+}
