97aaf6280bd4b4998ccd7e20d7650b5b07bd7013
JorgeRomojaro11 <jromodel@myuax.com>
2024-03-06 18:41:09 +0100

    tour para aficionados peces

---
+++ Zoologico/src/main/java/InteracciónConVisitantes/TourParaAficionadosPeces.java
@@ -0,0 +1,8 @@
+package InteracciónConVisitantes;
+
+public class TourParaAficionadosPeces extends Tour {
+    @Override
+    public void realizarTour() {
+        System.out.println("Bienvenidos aficionados a los peces! Vamos a explorar el mundo submarino.");
+    }
+}
